6d241888da094c3579ef4762ff49eb429f36f016
martinrisseeuw <hello@martinr.nl>
2017-04-13 15:31:02 +0200

    Backup

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -77,6 +77,22 @@
         </ul>
   </footer>
   <?php echo js('assets/js/output.min.js') ?>
+<!-- Piwik -->
+	<script type="text/javascript">
+	 var _paq = _paq || [];
+	 _paq.push(["setDomains", ["*.fablab.waag.org"]]);
+	 _paq.push(['trackPageView']);
+	 _paq.push(['enableLinkTracking']);
+	 (function() {
+	   var u="//stats.waag.org/";
+	   _paq.push(['setTrackerUrl', u+'piwik.php']);
+	   _paq.push(['setSiteId', 6]);
+	   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+	 })();
+	</script>
+	<noscript><p><img src="//stats.waag.org/piwik.php?idsite=6" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
 </div>
 </body>
 </html>
